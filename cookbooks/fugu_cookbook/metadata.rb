ec310a220badf17ef29ff741c3091cec7e2f3e80
eiskandar <eiskandar@bigpoint.net>
2015-12-14 18:58:30 +0100

    update cookbook

--- cookbooks/fugu_cookbook/metadata.rb
+++ cookbooks/fugu_cookbook/metadata.rb
@@ -5,3 +5,6 @@ license          'Apache 2.0'
 description      'Installs/Configures fugu_cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'build-essential'
+depends 'ntp'
